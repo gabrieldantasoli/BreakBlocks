de6de7456dee5fb7b3daf34d1112f144ddeffbc7
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-02-15 11:19:10 -0300

    Break Blocks

--- interations/script.js
+++ interations/script.js
@@ -8,6 +8,7 @@ var frames ;
 var bol = document.querySelector('#bol') ;
 var bar = document.querySelector('#ping') ;
 var withbol ;
+var lifes ;
 
 // Blocks
 var blockdiv = document.querySelector('#blocks') ;
@@ -19,7 +20,7 @@ var velbar ,dirbar ;
 var barWidth , posbarx ;
 
 // boll
-var posboly , posbolx , widthbol , heightbol , colorbol ;
+var posboly , posbolx , widthbol , heightbol , colorbol , velbol , dirbolx , dirboly ;
 
 // Ends variables
 
@@ -47,14 +48,18 @@ function controlbar() {
     if (playing) {
         posbarx += velbar * dirbar ;
         if (withbol) {
-            posbolx += velbar * dirbar ;
+            posbolx = posbarx
         } ;
         if (posbarx <= barWidth/2) {
             posbarx = barWidth/2 ;
-            posbolx = barWidth/2 ;
+            if (withbol) {
+                posbolx = barWidth/2 ;
+            } ;
         }else if (posbarx > 100-(barWidth/2)) {
             posbarx = 100-(barWidth/2) ;
-            posbolx = 100-(barWidth/2) ;
+            if (withbol) {
+                posbolx = 100-(barWidth/2) ;
+            } ;
         } ;
         bar.style.left = posbarx+'%' ;
         if (withbol) {
@@ -63,12 +68,73 @@ function controlbar() {
     } ;
 } ;
 
+function controlbol() {
+    posbolx += velbol * dirbolx ;
+    posboly += velbol * dirboly ;
+    // left and right wall colisions
+    if (posbolx >= 100-(withbol/2)) {
+        dirbolx *= -1 ;
+    }else if (posbolx <= 0+(withbol/2)) {
+        dirbolx *= -1 ;
+    }
+
+    // top and bottom wall colisions
+    if (posboly >= 100-(heightbol/2)) {
+        dirboly *= -1 ;
+    }else if (posboly <= 0 && playing) {
+        lifes -= 1 ;
+        replacebol() ;
+        replacebar() ;
+        replacelifes() ;
+        playing = false ;
+    } ;
+
+    // bar colisions
+    if ((posbolx+(widthbol/2) >= posbarx-(barWidth/2) && posboly <= 3) && (posbolx-(widthbol/2) <= posbarx+(barWidth/2))) {
+        dirboly *= -1 ;
+        dirbolx = (posbolx-posbarx)/6
+    } ;
+
+    bol.style.left = posbolx+'%' ;
+    bol.style.bottom = posboly+'%' ;
+} ;
+
 function game() {
     if (playing) {
-        //controlbol();
-        controlbar()
+        controlbar() ;
+        if (!withbol) {
+            controlbol() ;
+        }
     } ;
     frames = requestAnimationFrame(game) ;
+} ;
+
+function replacebol() {
+    posbolx = 50 ;
+    posboly = 3 ;
+    velbol = 1 ;
+    dirbolx = 0 ;
+    dirboly = 0 ;
+    widthbol = 3 ;
+    heightbol = 3 ;
+    withbol = true ;
+    bol.style.left = posbolx+'%' ;
+    bol.style.bottom = posboly+'%' ;
+    bol.style.height = heightbol+'%' ;
+    bol.style.width = widthbol+'%' ;
+    bol.style.background = colorbol ;
+} ;
+
+function replacebar() {
+    barWidth = 20 ;
+    posbarx = 50 ;
+    velbar = 1 ;
+    dirbar = 0 ;
+    document.getElementById('ping').style.left = posbarx+'%' ;
+} ;
+
+function replacelifes() {
+    document.querySelectorAll('.hearts i')[lifes].style.color = 'black' ;
 } ;
 
 //game ends
@@ -76,17 +142,12 @@ function game() {
 //Start variables
 
 function start() {
+    dirboly = 0 ;
+    dirbolx = 0 ;
+    withbol = true ;
     blocks = 100
     setBlocks(blocks) ;
-    barWidth = 20 ;
-    posbarx = 50 ;
-    posbolx = 50 ;
-    posboly = 3 ;
-    widthbol = 3 ;
-    heightbol = 3 ;
-    velbar = 1 ;
-    dirbar = 0 ;
-    withbol = true ;
+    lifes = 5 ;
     colorbol = 'var(--white)' ;
     document.addEventListener('keyup',function() {
         dirbar = 0 ;
@@ -98,15 +159,24 @@ function start() {
         }else if (key == 39) {
             dirbar = 1 ;
         } ;
+        if (key == 13) {
+            startGame()
+        }
     }) ;
-    document.getElementById('ping').style.left = posbarx+'%' ;
-    bol.style.left = posbolx+'%' ;
-    bol.style.bottom = posboly+'%' ;
-    bol.style.height = heightbol+'%' ;
-    bol.style.width = widthbol+'%' ;
-    bol.style.background = colorbol ;
+    replacebol() ;
+    replacebar() ;
     playing = true ;
     game() ;
+} ;
+
+function startGame() {
+    if (lifes > 0) {
+        dirboly = 1 ;
+        withbol = false ;
+        playing = true ;
+    }else {
+        alert('voce perdeu!')
+    }
 } ;
 
 // Starts Setting the blocks
@@ -121,9 +191,9 @@ function setBlocks() {
         block.style.height = '10%' ;
         let top = 10 * column ;
         let left = 10 * row ;
-        let color = colors[Math.round(Math.random() * 6)] ;
+        let color = colors[Math.round(Math.random() * 4)] ;
         block.style.background = `${color}` ;
-        block.style.border = '1px solid black' ;
+        block.style.outline = '1px solid black' ;
         block.style.top = top+'%' ;
         block.style.left = left+'%' ;
         if (built >= 100) {
